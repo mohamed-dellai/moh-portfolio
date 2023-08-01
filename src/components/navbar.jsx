215efd6092d778246318e98d26f94f48047805d9
mohamed-dellai <mohameddellai6@gmail.com>
2023-08-01 17:59:32 +0100

    h

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 export function NavBar() {
     return (
        <nav id='navbar'>
-           <a href='#about' className='link'>out</a>
+           <a href='#about' className='link'>ut</a>
           <a href='#work' className='link'>Work</a>
           <a href='#footer' className='link'>Contact</a>
        </nav>
